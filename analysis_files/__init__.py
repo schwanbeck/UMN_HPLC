f535f79e5130441478086af9898ab0fe5315f74e
schwanbeck <schwan@umn.edu>
2024-04-22 17:50:24 -0500

    Added individual analysis files

---
+++ analysis_files/__init__.py
@@ -0,0 +1,9 @@
+#
+"""
+   Copyright (c) 2024. Julian Schwanbeck.
+
+   This file contains the This file is part of it under the terms of the GNU General Public License as published by the Free Software Foundation,
+   either version 3 of the License, or (at your option) any later version. the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+   details. You should have received a copy of the GNU General Public License along with not, see <http://www.gnu.org/licenses/>.
+   """
